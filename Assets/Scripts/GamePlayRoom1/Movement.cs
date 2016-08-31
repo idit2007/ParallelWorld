842dae23f44b98f3bfdb58e8e78a040e48f93dc0
idit2007 <ItBangchokdee@Its-MacBook-Pro.local>
2016-08-31 14:02:38 +0700

    effect teleportion

---
+++ Assets/Scripts/GamePlayRoom1/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Movement : MonoBehaviour {
+	public float speed = 0.001f;
+	public bool isMoving;
+	public Transform[] targetList;
+	public float cntStep = 0;
+	public int indexTarget = 0;
+	// Use this for initialization
+	void Start () {
+		isMoving = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isMoving)
+		{
+			cntStep += Time.deltaTime * speed;
+			transform.position = Vector3.MoveTowards(transform.position, targetList[indexTarget].position, cntStep);
+			var rotation = Quaternion.LookRotation(targetList[indexTarget].position - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+			if (Vector3.Distance(transform.position, targetList[indexTarget].position) < 0.001f)
+			{
+				transform.LookAt(targetList[indexTarget]);
+				indexTarget++;
+				if (indexTarget >= targetList.Length)
+				{
+					transform.rotation = targetList[indexTarget - 1].rotation;
+					isMoving = false;
+					targetList = null;
+
+
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/GamePlayRoom1/Teleportion.cs
+++ Assets/Scripts/GamePlayRoom1/Teleportion.cs
@@ -9,15 +9,20 @@ public class Teleportion : MonoBehaviour {
 	private GameObject particleTeleportionStart;
 	private GameObject particleTeleportionStop;
 	private GameObject blueFlash;
+	private GameObject explosionLight;
+	private GameObject teleportButton;
 	// Use this for initialization
 	void Start () {
 		world = true;
+		teleportButton = GameObject.Find ("TeleportionButton");
+	
 		Player = GameObject.FindGameObjectWithTag("Player");
 		CameraSet = GameObject.Find ("CameraSet");
 		CameraSet.SetActive (false);
 		particleTeleportionStart = GameObject.Find ("TeleportStart");
 		particleTeleportionStop= GameObject.Find ("TeleportStop");
 		blueFlash = GameObject.Find ("BlueFlash");
+		explosionLight = GameObject.Find ("ExplosionLight");
 		particleTeleportionStart.SetActive (false);
 		particleTeleportionStop.SetActive (false);
 		blueFlash.SetActive (false);
@@ -34,6 +39,8 @@ public class Teleportion : MonoBehaviour {
 	}
 	IEnumerator TeleportAnimation()
 	{
+		Button teleportButtonC = teleportButton.GetComponent<Button>();
+		teleportButtonC.interactable = false;
 		particleTeleportionStart.SetActive (true);
 		blueFlash.SetActive (true);
 		yield return new WaitForSeconds (2f);
@@ -49,10 +56,11 @@ public class Teleportion : MonoBehaviour {
 			Player.transform.position = new Vector3 (Player.transform.position.x - teleportRange, Player.transform.position.y, Player.transform.position.z);
 			CameraSet.SetActive (false);
 		}
+		explosionLight.SetActive (false);
 		particleTeleportionStop.SetActive (true);
 		yield return new WaitForSeconds (4f);
-	
 		particleTeleportionStart.SetActive (false);
 		particleTeleportionStop.SetActive (false);
+		teleportButtonC.interactable = true;
 	}
 }
