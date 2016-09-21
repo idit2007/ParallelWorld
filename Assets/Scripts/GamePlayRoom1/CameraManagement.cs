1773b31b512fb1e29fa1267dd68510d04f98ef1a
idit2007 <ItBangchokdee@Its-MacBook-Pro.local>
2016-09-21 17:29:01 +0700

    Camera Management

---
+++ Assets/Scripts/GamePlayRoom1/CameraManagement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManagement : MonoBehaviour {
+	public GameObject thirdPersonCamera;
+	// Use this for initialization
+	void Start () {
+		thirdPersonCamera = GameObject.Find ("ThirdPersonCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TurnController.Instance.playerMovemnet)
+			thirdPersonCamera.SetActive (true);
+		else
+			thirdPersonCamera.SetActive (false);
+	
+	}
+}
