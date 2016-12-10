24ff3af73ff11f470b5c5d36e5f770932b32eb95
tadds <tadgm@hotmail.com>
2016-12-10 22:18:42 +0700

    minimap

--- Assets/Scripts/GamePlayRoom1/MapController.cs
+++ Assets/Scripts/GamePlayRoom1/MapController.cs
@@ -4,25 +4,24 @@ using System.Collections;
 public class MapController : MonoBehaviour
 {
     private Animator anim;
-    public GameObject[] BigMap;
+    public GameObject BigMap;
     // Use this for initialization
     void Start()
     {
         anim = GameObject.Find("MapFeild").GetComponent<Animator>();
-        BigMap[0].SetActive(false);
+        BigMap.SetActive(false);
     }
 
 
     public void MapButtonBig()
     {
         anim.SetTrigger("Big");
-        BigMap[0].SetActive(true);
-        Debug.Log("a");
+        BigMap.SetActive(true);
     }
 
     public void MapButtonSmall()
     {
         anim.SetTrigger("Small");
-        BigMap[0].SetActive(false);
+        BigMap.SetActive(false);
     }
 }
